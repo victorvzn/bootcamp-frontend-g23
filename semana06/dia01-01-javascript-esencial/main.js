0dc2b1f67d3b942ee3d1607d1c3141a82ef17663
Victor Villazón <victorzn23@gmail.com>
2025-01-15 19:59:47 -0500

    semana06, dia01, javascript esencial

--- semana06/dia01-01-javascript-esencial/main.js
+++ semana06/dia01-01-javascript-esencial/main.js
@@ -1,1 +1,67 @@
-console.log('Hola a todos!')
+// COMENTARIOS
+
+// console.log('Hola a todos!')
+
+/*
+  Esto
+  es
+  un
+  comentario
+  de
+  múltiples
+  líneas
+*/
+
+// TIPOS DE DATOS
+
+// PRIMITIVOS: Number, String, Boolean(true, false), undefined, null, BigInt, Symbol
+
+// NUMBER
+
+console.log(20)
+console.log(15.69)
+console.log(0.999)
+console.log(-36)
+
+console.log(typeof 20) // number
+console.log(typeof 15.69) // number
+console.log(typeof 0.999) // number
+console.log(typeof -36) // number
+
+// STRING (Cadenas de texto)
+
+console.log('Cesar')
+console.log("Carlos")
+console.log("2000")
+console.log("🏀")
+
+console.log(typeof 'Cesar') // string
+console.log(typeof "Carlos") // string
+console.log(typeof "2000") // string
+console.log(typeof "🏀") // string
+
+// BOOLEAN -> true or false
+
+console.log(true)
+console.log(false)
+
+console.log(typeof true) // boolean
+console.log(typeof false) // boolean
+
+// UNDEFINED
+
+let nombre // Declaración de una variable
+console.log(nombre)
+console.log(typeof nombre)
+
+// NULL
+
+let apellido = null
+console.log(apellido) // null
+console.log(typeof apellido) // object === null
+
+// BigInt (Permite almacenar número muy grandes)
+// Symbol (Ayudan a crear valores únicos e irrepetibles)
+
+
+// VARIABLES Y CONSTANTES

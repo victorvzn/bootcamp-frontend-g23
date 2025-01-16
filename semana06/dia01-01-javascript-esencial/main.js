7e05a19dba7b3e91fd76543bccbbce4272249309
Victor Villazón <victorzn23@gmail.com>
2025-01-15 22:35:49 -0500

    semana06, dia01, javascript esencial

--- semana06/dia01-01-javascript-esencial/main.js
+++ semana06/dia01-01-javascript-esencial/main.js
@@ -266,3 +266,86 @@ switch (heroe) {
 
 // ESTRUCTURAS REPETITIVAS
 
+// FOR (Sirve para repetir una o varias instrucciones)
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+// WHILE
+
+let j = 0
+
+while (j < 10) {
+  console.log('while', j)
+  // j = j + 1
+  j++
+}
+
+// DO WHILE
+
+let k = 0
+
+do {
+  console.log('do while', k)
+  // k = k + 1
+  k++
+} while (k < 10)
+
+// EJERCICIOS
+
+// 1. Determinr si la edad de un persona es mayor de edad mostrando 'Mayor de edad' o 'Menor de edad' según corresponda.
+
+const edadPersona = 15
+
+if (edadPersona >= 18) {
+  console.log('Mayor de edad')
+} else {
+  console.log('Menor de edad')
+}
+
+// 2. Retornar un saludo en tres diferentes lenguajes:
+//    - si es español mostrará 'Hola'
+//    - si es inglés mostrará 'Hello'
+//    - si es aimara mostrará 'kamisaraki'
+// 3. Si un numero es divisible entre 3 y 5 devolver 'fizzbuzz'
+//    Si un numero es divisible entre 3 devolver 'fizz'
+//    Si un numero es divisible entre 5 devolver 'buzz'
+//    De lo contrario devolver el mismo número
+// 4. Retornar true si un numero es primo sino devolver false
+//    Pista: un numero primo es divisible por sí mismo y por 1
+// 5. Devolver un valor que se incremente de dos en dos y devuelva los menores a 10
+
+
+// FUNCIONES (Bloques de código que podemos reutilizar)
+
+// Declaración
+function nombreDeLaFuncion() {
+  // Cuerpo de la función y aquí va lógica a ejecutar y reutilizar
+}
+
+// Ejecutar, aquí estamos llamando a la función
+nombreDeLaFuncion()
+nombreDeLaFuncion()
+
+// FUNCIONES Sin parámetros
+
+function imprimirMiNombre() {
+  console.log('Hola soy Victor')
+  console.log('Y soy frontend')
+}
+
+imprimirMiNombre()
+
+// FUNCIONES con parámetros y valores por defecto
+
+function imprimirUnNombre(nombre = 'Anónimo') {
+  // if (nombre === undefined) {
+  //   nombre = 'Anónimo'
+  // }
+
+  console.log('Hola soy ' + nombre)
+}
+
+imprimirUnNombre()
+imprimirUnNombre('Victor')

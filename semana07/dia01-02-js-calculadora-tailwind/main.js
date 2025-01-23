7ff16823c23f96d98164f4909c3e38bfe4e4232b
Victor Villazón <victorzn23@gmail.com>
2025-01-22 21:36:41 -0500

    semana07, dia01, javascript eventos dom calculadora tailwind

--- semana07/dia01-02-js-calculadora-tailwind/main.js
+++ semana07/dia01-02-js-calculadora-tailwind/main.js
@@ -1,4 +1,23 @@
+let numeroActual = '0'
+let operador = '' // + * -
+let operado = '' // Cualquier número. Ejemplo: 9,
+
 const inputDisplay = document.querySelector('.inputDisplay')
 const buttons = document.querySelectorAll('button')
 
-console.log({ inputDisplay, buttons })
+// console.log({ inputDisplay, buttons })
+
+buttons.forEach(function(button) {
+  // console.log({button})
+
+  button.addEventListener('click', function(event) {
+    // console.log({event: event.target})
+    const buttonText = event.target.textContent
+
+    console.log(buttonText)
+
+    // TODO: Añadimos la lógica de la calculadora
+
+    
+  })
+})

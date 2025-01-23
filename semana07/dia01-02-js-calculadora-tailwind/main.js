ad97a72d81a233efe07c5810465ef1e8e0e64384
Victor Villazón <victorzn23@gmail.com>
2025-01-22 21:57:38 -0500

    semana07, dia01, javascript eventos dom calculadora tailwind

--- semana07/dia01-02-js-calculadora-tailwind/main.js
+++ semana07/dia01-02-js-calculadora-tailwind/main.js
@@ -1,6 +1,6 @@
 let numeroActual = '0'
 let operador = '' // + * -
-let operado = '' // Cualquier número. Ejemplo: 9,
+let operando = '' // Cualquier número. Ejemplo: 9,
 
 const inputDisplay = document.querySelector('.inputDisplay')
 const buttons = document.querySelectorAll('button')
@@ -18,6 +18,33 @@ buttons.forEach(function(button) {
 
     // TODO: Añadimos la lógica de la calculadora
 
-    
+    if ('+-*'.includes(buttonText)) {
+      // Asignamos el operador actual
+      console.log('presionaste el operador', buttonText)
+      operador = buttonText
+      operando = Number(numeroActual)
+      numeroActual = '0'
+    } else if (buttonText === '=') {
+      // Aquí realizo las operaciones matemáticas en base al número actual y al operando
+      // console.log('presionaste el signo', buttonText)
+      if (operador === '+') {
+        numeroActual = Number(operando) + Number(numeroActual)
+      } else if (operador === '-') {
+        numeroActual = Number(operando) - Number(numeroActual)
+      } else if (operador === '*') {
+        numeroActual = Number(operando) * Number(numeroActual)
+      }
+    } else if (buttonText === 'CE') {
+      console.log('presionaste el botón', buttonText)
+      numeroActual = '0'
+      operador = ''
+      operando = ''
+    } else {
+      // 01- Cuando hemos presionado solo los números
+      console.log('presionaste el número', buttonText)
+      numeroActual = numeroActual + buttonText
+    }
+
+    inputDisplay.value = Number(numeroActual)
   })
 })

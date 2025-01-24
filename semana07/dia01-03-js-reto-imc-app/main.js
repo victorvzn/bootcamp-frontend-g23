f6b02737d5b843583ed51be24aa1b3e7661dcd90
Victor Villazón <victorzn23@gmail.com>
2025-01-23 19:29:31 -0500

    semana07, dia01, javascript reto imc app (dia02 continuación)

--- semana07/dia01-03-js-reto-imc-app/main.js
+++ semana07/dia01-03-js-reto-imc-app/main.js
@@ -1,1 +1,11 @@
-console.log('Hola JS')
+// const buttonCalcular = document.querySelector('#calcular')
+// console.log({buttonCalcular})
+
+const inputPeso = document.getElementById('peso')
+const inputAltura = document.getElementById('altura')
+const buttonCalcular = document.getElementById('calcular')
+const sectionResultado = document.getElementById('resultado')
+
+buttonCalcular.addEventListener('click', function(event) {
+  console.log('click...')
+})

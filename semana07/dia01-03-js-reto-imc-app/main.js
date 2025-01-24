14462f123c5113c6bb6c9141473601b7b5cd19d2
Victor Villazón <victorzn23@gmail.com>
2025-01-23 19:47:35 -0500

    semana07, dia01, javascript reto imc app (dia02 continuación)

--- semana07/dia01-03-js-reto-imc-app/main.js
+++ semana07/dia01-03-js-reto-imc-app/main.js
@@ -7,5 +7,27 @@ const buttonCalcular = document.getElementById('calcular')
 const sectionResultado = document.getElementById('resultado')
 
 buttonCalcular.addEventListener('click', function(event) {
-  console.log('click...')
+  // console.log('click...')
+
+  const peso = inputPeso.value
+  const altura = inputAltura.value
+
+  // console.log(peso, altura)
+
+  // IMC = peso / altura * altura
+  const indiceMasaCorporal = peso / ((altura / 100) ** 2)
+
+  console.log(indiceMasaCorporal)
+
+  let resultado = ''
+
+  // TODO: Mostrar el resultado según el IMC
+
+  console.log(resultado)
 })
+
+
+// * Baja: < 18.5
+// * Normal: < 18.5 - 24.9
+// * Sobrepeso: < 25 - 29.9 
+// * Obeso: > 30

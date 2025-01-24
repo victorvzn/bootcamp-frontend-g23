f0d64da9c1a588826e766dd89244beaca0340308
Victor Villazón <victorzn23@gmail.com>
2025-01-23 21:37:09 -0500

    semana07, dia02, javascript todo app simple v1

--- semana07/dia02-01-js-todo-app-simple-v1/main.js
+++ semana07/dia02-01-js-todo-app-simple-v1/main.js
@@ -7,6 +7,14 @@ taskAdd.addEventListener('click', function(event) {
 
   console.log('añadiendo tarea...', taskInput.value)
 
+  const elementLi = document.createElement('li')
+
+  elementLi.textContent = taskInput.value
+
+  // TODO: Añadir un botón al li con el texto 'Borrar'
+
+  taskList.appendChild(elementLi)
+
   // const button = document.createElement('button')
   // button.textContent = '¡Hola soy un botón!'
   // console.log(button)

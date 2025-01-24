cd95dbd38813d8d5883106b6caccacae7fdba073
Victor Villazón <victorzn23@gmail.com>
2025-01-23 21:00:00 -0500

    semana07, dia02, javascript todo app simple v1

--- semana07/dia02-01-js-todo-app-simple-v1/main.js
+++ semana07/dia02-01-js-todo-app-simple-v1/main.js
@@ -1,1 +1,14 @@
-console.log('hola js')
+const taskInput = document.querySelector('.task__input')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
+
+  console.log('añadiendo tarea...', taskInput.value)
+
+  // const button = document.createElement('button')
+  // button.textContent = '¡Hola soy un botón!'
+  // console.log(button)
+  // document.body.appendChild(button)
+})

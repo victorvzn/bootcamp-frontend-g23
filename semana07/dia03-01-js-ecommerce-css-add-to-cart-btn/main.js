16f86b814c5ef09303bd87959f9bc8673c9865e4
Victor Villazón <victorzn23@gmail.com>
2025-01-24 19:47:37 -0500

    semana07, dia03, javascript ecommerce add to cart

--- semana07/dia03-01-js-ecommerce-css-add-to-cart-btn/main.js
+++ semana07/dia03-01-js-ecommerce-css-add-to-cart-btn/main.js
@@ -1,1 +1,23 @@
-console.log('Hola JS')
+// const btnMenos = document.getElementById('btnMenos')
+const btnMenos = document.querySelector('#btnMenos')
+const btnMas = document.querySelector('#btnMas')
+const cantidad = document.querySelector('#cantidad')
+
+let contador = 0
+
+btnMas.addEventListener('click', function(event) {
+  contador = contador + 1
+
+  console.log(contador)
+
+  // DONE: Validar que el número sea como máximo 10
+
+  if(contador > 10) {
+    contador = 10
+    return
+  }
+
+  cantidad.value = contador
+})
+
+// TODO: Añadir el evento click al botón btnMenos y validar que no se ingresen números menores a 1

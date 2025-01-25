d2f832ec4feabb18ddb55ed6a9a7240c31456f25
Victor Villazón <victorzn23@gmail.com>
2025-01-24 21:09:12 -0500

    semana07, dia03, javascript trivia app tailwind

--- semana07/dia03-02-js-trivia-app-reto-tailwind/main.js
+++ semana07/dia03-02-js-trivia-app-reto-tailwind/main.js
@@ -32,3 +32,45 @@ const questions = [
 let correctAnswerCounter = 0 // Contador
 let currentQuestionIndex = 0
 
+const questionsAndResults = document.querySelector('#questions-and-results')
+
+function renderQuestions() {
+  const currentQuestion = questions[currentQuestionIndex]
+
+  console.log(currentQuestion)
+
+  questionsAndResults.innerHTML = `
+    <section class="flex flex-col px-4 py-6 border border-gray-300 rounded-lg shadow">
+      <!-- Aqui van las preguntas -->
+      <p class="text-md font-medium text-gray-800 mb-4">
+        ${currentQuestion.order}. ${currentQuestion.question}
+      </p>
+
+      <div class="flex flex-col items-start mb-10 gap-2">
+        <button class="border border-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-blue-700 text-left hover:bg-blue-800 hover:text-white cursor-pointer">
+          ${currentQuestion.answerList[0]}
+        </button>
+        <button class="border border-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-blue-700 text-left hover:bg-blue-800 hover:text-white cursor-pointer">
+          ${currentQuestion.answerList[1]}
+        </button>
+        <button class="border border-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-blue-700 text-left hover:bg-blue-800 hover:text-white cursor-pointer">
+          ${currentQuestion.answerList[2]}
+        </button>
+        <button class="hidden border border-red-500 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-white text-left cursor-pointer bg-red-500">Respuesta incorrecta</button>
+        <button class="hidden border border-green-500 font-medium rounded-lg text-sm px-5 py-2.5 w-full text-white text-left cursor-pointer bg-green-500">Respuesta correcta</button>
+      </div>
+
+      <div class="flex justify-end gap-2">
+        <button class="hidden text-gray-800 bg-white hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 cursor-pointer">
+          <img src="./images/icon-arrow-left.svg" width="32" height="32" />
+        </button>
+        <button class="text-gray-800 bg-white hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 cursor-pointer">
+          <img src="./images/icon-arrow-right.svg" width="32" height="32" />
+        </button>
+        <button class="hidden text-gray-800 bg-white hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 cursor-pointer">Ver Resultados</button>
+      </div>
+    </section>
+  `
+}
+
+renderQuestions()

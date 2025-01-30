2eb5d09a2767df26c57a93f046542d92967dc003
Victor Villazón <victorzn23@gmail.com>
2025-01-29 21:37:46 -0500

    semana08, dia01, javascript + es6+

--- semana08/dia01-02-javascript-hangman-game/main.js
+++ semana08/dia01-02-javascript-hangman-game/main.js
@@ -11,4 +11,20 @@ const ALPHABET = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65
 let correctGuess = ''
 let tries = 0
 
-//
+// const renderMaskedWord = () => {
+//   elMaskedWord.textContent = 
+// }
+
+// DONE: Implementar una funcion para renderizar los botones con cada letra del alfabeto de la A a la Z.
+
+const renderAlphabet = () => {
+  let list = ''
+
+  ALPHABET.forEach(letter => {
+    list += `<button class="p-2 bg-blue-500 text-white font-bold text-xl cursor-pointer hover:bg-blue-400 duration-300">${letter}</button>`
+  })
+
+  elButtons.innerHTML = list
+}
+
+renderAlphabet()

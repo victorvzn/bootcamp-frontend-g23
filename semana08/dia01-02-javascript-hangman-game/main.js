0a8c5dcae2fa4f7efeaeadd2555580855bfed62e
Victor Villazón <victorzn23@gmail.com>
2025-01-29 21:55:45 -0500

    semana08, dia01, javascript + es6+

--- semana08/dia01-02-javascript-hangman-game/main.js
+++ semana08/dia01-02-javascript-hangman-game/main.js
@@ -11,20 +11,43 @@ const ALPHABET = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65
 let correctGuess = ''
 let tries = 0
 
-// const renderMaskedWord = () => {
-//   elMaskedWord.textContent = 
-// }
-
 // DONE: Implementar una funcion para renderizar los botones con cada letra del alfabeto de la A a la Z.
 
 const renderAlphabet = () => {
   let list = ''
 
   ALPHABET.forEach(letter => {
-    list += `<button class="p-2 bg-blue-500 text-white font-bold text-xl cursor-pointer hover:bg-blue-400 duration-300">${letter}</button>`
+    list += `<button class="p-2 bg-blue-500 text-white font-bold text-xl cursor-pointer hover:bg-blue-400 duration-300" onclick="selectLetter('${letter}')">${letter}</button>`
   })
 
   elButtons.innerHTML = list
+}
+
+const selectLetter = (letter) => {
+  tries++
+
+  console.log(tries)
+
+  console.log('seleccionando la letra', letter)
+
+  correctGuess = correctGuess + letter
+
+  console.log('intentos', correctGuess)
+
+  renderMaskedWord()
+}
+
+const renderMaskedWord = () => {
+  // console.log('rendering masked word')
+
+  elMaskedWord.textContent = word
+    .split('')
+    .map(
+      letter => correctGuess.includes(letter) ? letter : '_'
+    )
+    .join('')
+
+  // TODO: Si gana el juego hay que agregar la lógica respectiva
 }
 
 renderAlphabet()

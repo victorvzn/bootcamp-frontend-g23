0a88924977598771be24e99ed7f81cac1524a737
Victor Villazón <victorzn23@gmail.com>
2025-01-30 19:44:17 -0500

    semana08, dia01, javascript + word counter reto

--- semana08/dia02-01-javascript-fetch/main.js
+++ semana08/dia02-01-javascript-fetch/main.js
@@ -1,1 +1,10 @@
-// console.log('Hola JS')
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+// OBJETIVO: Traer información en formato JSon desde una URL
+
+fetch(url) // Devuelve una promesa (Promise)
+  .then(response => response.json()) // Convertimos la respuesta a un objeto JS
+  .then(data => {
+    console.log(data) // Aquí podemos usar el objeto JS
+    console.log(data[0].title)
+  })

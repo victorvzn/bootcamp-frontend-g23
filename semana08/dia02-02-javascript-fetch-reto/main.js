1db100015fdfe1656b48ce06b9efa8eab19c1dc6
Victor Villazón <victorzn23@gmail.com>
2025-01-30 20:35:45 -0500

    semana08, dia01, javascript + word counter reto

--- semana08/dia02-02-javascript-fetch-reto/main.js
+++ semana08/dia02-02-javascript-fetch-reto/main.js
@@ -1,3 +1,32 @@
 // DOCUMENTACIÓN: https://web.dragonball-api.com/
 
 // API: https://dragonball-api.com/api/characters
+
+const url = 'https://dragonball-api.com/api/characters'
+
+const renderCharacters = (characters = []) => {
+  const divApp = document.querySelector('#app')
+
+  let characterList = ''
+
+  characters.forEach(character => {
+    characterList += `
+      <article>
+        <h2>${character.name}<h2>
+        <img src="${character.image}" height="200" />
+      </article>
+    `
+  })
+
+  divApp.innerHTML = characterList
+}
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    renderCharacters(data.items)
+  })
+
+
+// TODO: Añadir al renderizado de characters una paginación agregando botón anterior y siguiente

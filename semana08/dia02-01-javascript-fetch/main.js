26adc1c68bed15c61449f592c82c65113b943a0e
Victor Villazón <victorzn23@gmail.com>
2025-01-30 19:57:23 -0500

    semana08, dia01, javascript + word counter reto

--- semana08/dia02-01-javascript-fetch/main.js
+++ semana08/dia02-01-javascript-fetch/main.js
@@ -2,9 +2,33 @@ const url = 'https://jsonplaceholder.typicode.com/posts'
 
 // OBJETIVO: Traer información en formato JSon desde una URL
 
-fetch(url) // Devuelve una promesa (Promise)
-  .then(response => response.json()) // Convertimos la respuesta a un objeto JS
+// fetch(url) // Devuelve una promesa (Promise)
+//   .then(response => response.json()) // Convertimos la respuesta a un objeto JS
+//   .then(data => {
+//     console.log(data) // Aquí podemos usar el objeto JS
+//     console.log(data[0].title)
+//   })
+
+const renderPosts = (posts = []) => {
+  const divApp = document.querySelector('#app')
+
+  let postList = ''
+
+  posts.forEach(post => {
+    postList += `
+      <article>
+        <h2>${post.id} - ${post.title}</h2>
+        <p>${post.body}</p>
+      </article>
+    `
+  })
+
+  divApp.innerHTML = postList
+}
+
+fetch(url)
+  .then(response => response.json())
   .then(data => {
-    console.log(data) // Aquí podemos usar el objeto JS
-    console.log(data[0].title)
+    console.log(data)
+    renderPosts(data)
   })

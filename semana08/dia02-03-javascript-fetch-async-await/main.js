bcb1760f2e1f3dade9aa63f0bc68ab5f8c995955
Victor Villazón <victorzn23@gmail.com>
2025-01-30 21:40:22 -0500

    semana08, dia01, javascript fetch api, async await

--- semana08/dia02-03-javascript-fetch-async-await/main.js
+++ semana08/dia02-03-javascript-fetch-async-await/main.js
@@ -16,7 +16,44 @@ const fetchPostsConRetorno = async () => { // Retorno una promesa (Promise)
   return await response.json()
 }
 
+const fetchPostsConManejoDeErrores = async () => {
+  try {
+    const response = await fetch(url)
+
+    console.log(response)
+
+    if (!response.ok) { // ok === STATUS CODE 200
+      console.log('Tuvimos problemas para cargar el recurso posts.')
+      throw new Error('ERROR HTTP: ' + response.status)
+    }
+
+    return await response.json()
+  } catch(error) { // Aquí se evaluan los errores inesperados
+    console.log('>>>', error)
+  }
+}
+
+const renderPosts = (posts = []) => {
+  const divApp = document.querySelector('#app')
+
+  let postList = ''
+
+  posts.forEach(post => {
+    postList += `
+      <div>
+        <h2>${post.id} - ${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `
+  })
+
+  divApp.innerHTML = postList
+}
+
 // fetchPostsSinRetorno()
 
-fetchPostsConRetorno()
-  .then(posts => console.log(posts))
+// fetchPostsConRetorno()
+//   .then(posts => console.log(posts))
+
+fetchPostsConManejoDeErrores()
+  .then(posts => renderPosts(posts))

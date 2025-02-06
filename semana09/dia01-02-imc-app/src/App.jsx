ac32117b8870222435b3bdb10cc72053d6a06116
Victor Villazón <victorzn23@gmail.com>
2025-02-05 21:39:07 -0500

    semana09, dia01, react imc app

--- semana09/dia01-02-imc-app/src/App.jsx
+++ semana09/dia01-02-imc-app/src/App.jsx
@@ -1,8 +1,32 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      IMC Calculator
-    </h1>
+    <section className="w-[400px] mx-auto bg-slate-200 p-4 mt-8">
+      <h1 className="text-3xl font-bold text-red-600 text-center">
+        IMC Calculator
+      </h1>
+
+      <div className="pt-4">
+        <p>Weight: 0 kg</p>
+        <input
+          type="range"
+          min="50"
+          max="200"
+        />
+      </div>
+
+      <div className="pt-4">
+        <p>Height: 0 cm</p>
+        <input
+          type="range"
+          min="50"
+          max="200"
+        />
+      </div>
+
+      <p className="font-bold">Tu IMC es 0.00</p>
+
+      <p className="font-bold text-2xl">Estado de IMC: ????</p>
+    </section>
   )
 }
 

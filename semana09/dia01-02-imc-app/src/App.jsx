75b2891647d4127bf97037f87f0bdcd7a3eec3f8
Victor Villazón <victorzn23@gmail.com>
2025-02-05 22:11:59 -0500

    semana09, dia01, react imc app

--- semana09/dia01-02-imc-app/src/App.jsx
+++ semana09/dia01-02-imc-app/src/App.jsx
@@ -9,8 +9,42 @@ const App = () => {
   // const getWeight = weight[0]
   // const setWeight = weight[1]
 
-  const [weight, setWeight] = useState(1)
-  const [height, setHeight] = useState(1)
+  const [weight, setWeight] = useState(50)
+  const [height, setHeight] = useState(50)
+
+  const handleWeight = (event) => {
+    setWeight(event.target.value)
+  }
+
+  const handleHeight = (event) => setHeight(event.target.value)
+
+  const imc = weight / ((height / 100) ** 2)
+
+  const imcToDecimal = imc.toFixed(2)
+
+  // * Baja: < 18.5
+  // * Normal: < 18.5 - 24.9
+  // * Sobrepeso: < 25 - 29.9 
+  // * Obeso: > 30
+
+  const imcResult = () => {
+    let result = ''
+    console.log(imc)
+
+    if (imc < 18.5) {
+      result = 'Baja'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      result = 'Normal'
+    } else if (imc >= 25 && imc <= 29.9) {
+      result = 'Sobrepeso'
+    } else if (imc > 30) {
+      result = 'Obeso'
+    } else {
+      result = 'Datos incorrectos'
+    }
+
+    return result
+  }
 
   return (
     <section className="w-[400px] mx-auto bg-slate-200 p-4 mt-8">
@@ -24,7 +58,8 @@ const App = () => {
           type="range"
           min="50"
           max="200"
-          onChange={(event) => setWeight(event.target.value)}
+          onChange={handleWeight}
+          value={weight}
         />
       </div>
 
@@ -33,14 +68,15 @@ const App = () => {
         <input
           type="range"
           min="50"
-          max="200"
-          onChange={(event) => setHeight(event.target.value)}
+          max="1000"
+          onChange={handleHeight}
+          value={height}
         />
       </div>
 
-      <p className="font-bold">Tu IMC es 0.00</p>
+      <p className="font-bold">Tu IMC es {imcToDecimal}</p>
 
-      <p className="font-bold text-2xl">Estado de IMC: ????</p>
+      <p className="font-bold text-2xl">Estado de IMC: {imcResult()}</p>
     </section>
   )
 }

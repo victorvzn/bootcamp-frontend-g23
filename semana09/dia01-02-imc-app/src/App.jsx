f7644606459ae8cfc8d5f0c9510e38fd67377e7f
Victor Villazón <victorzn23@gmail.com>
2025-02-05 21:55:19 -0500

    semana09, dia01, react imc app

--- semana09/dia01-02-imc-app/src/App.jsx
+++ semana09/dia01-02-imc-app/src/App.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react"
+
 const App = () => {
+  // useState => Este hook nos ayuda a generar un estado para manipular datos dentro de nuestro componente
+  // useState => devuelve un arreglo con una variable de lectura y una función de escritura para cambiar el estado
+
+  // const weight = useState(1)
+
+  // const getWeight = weight[0]
+  // const setWeight = weight[1]
+
+  const [weight, setWeight] = useState(1)
+  const [height, setHeight] = useState(1)
+
   return (
     <section className="w-[400px] mx-auto bg-slate-200 p-4 mt-8">
       <h1 className="text-3xl font-bold text-red-600 text-center">
@@ -6,20 +19,22 @@ const App = () => {
       </h1>
 
       <div className="pt-4">
-        <p>Weight: 0 kg</p>
+        <p>Weight: {weight} kg</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={(event) => setWeight(event.target.value)}
         />
       </div>
 
       <div className="pt-4">
-        <p>Height: 0 cm</p>
+        <p>Height: {height} cm</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={(event) => setHeight(event.target.value)}
         />
       </div>
 

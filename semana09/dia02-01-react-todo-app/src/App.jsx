f8318e9157c77a8a6573c8c0f5d791b56cfd8597
Victor Villazón <victorzn23@gmail.com>
2025-02-06 20:04:25 -0500

    semana09, dia02, react todo app + tw

--- semana09/dia02-01-react-todo-app/src/App.jsx
+++ semana09/dia02-01-react-todo-app/src/App.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react"
 import TodoHeader from "./components/TodoHeader"
 
 const App = () => {
+  const DEFAULT_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender Javascript',
+      completed: true
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js + Tailwind',
+      completed: true
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+
+  console.log(todos)
+
   return (
-    <main>
+    <main
+      className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-lg p-4"
+    >
       <TodoHeader title='TODO APP + React + Tailwind' color='text-blue-500' />
+
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>{todo.title}</li>
+          )
+        })}
+      </ul>
+
+      <pre className="bg-slate-100 p-4 mt-8">{JSON.stringify(todos, null, 2)}</pre>
     </main>
   )
 }
--- semana09/dia02-01-react-todo-app/src/components/TodoHeader.jsx
+++ semana09/dia02-01-react-todo-app/src/components/TodoHeader.jsx
@@ -1,7 +1,7 @@
 const TodoHeader = ({ title, color }) => {
   return (
     <header>
-      <h1 className={`text-3xl font-bold underline ${color}`}>
+      <h1 className={`text-xl font-bold underline ${color}`}>
         {title}
       </h1>
     </header>

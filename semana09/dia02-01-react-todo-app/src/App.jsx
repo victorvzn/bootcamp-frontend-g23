049cdeb32ab0e27dbf2ae23fc0169c944e430422
Victor Villazón <victorzn23@gmail.com>
2025-02-06 20:45:29 -0500

    semana09, dia02, react todo app + tw

--- semana09/dia02-01-react-todo-app/src/App.jsx
+++ semana09/dia02-01-react-todo-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import TodoHeader from "./components/TodoHeader"
+import TodoList from "./components/TodoList"
 
 const App = () => {
   const DEFAULT_TODOS = [
@@ -51,28 +52,7 @@ const App = () => {
     >
       <TodoHeader title='TODO APP + React + Tailwind' color='text-blue-500' />
 
-      <ul className="flex flex-col gap-2 mt-4">
-        {todos.map(todo => {
-          return (
-            <li key={todo.id} className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
-              <input
-                type="checkbox"
-                data-id={todo.id}
-                checked={todo.completed}
-                onChange={handleCompleted}
-              />
-              <div className="w-full flex justify-between items-center">
-                <span className={`font-medium ${todo.completed ? 'line-through' : ''}`}>
-                  {todo.title}
-                </span>
-                <button className="bg-red-300 rounded-lg px-1 py-1 cursor-pointer">
-                  ❌
-                </button>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <TodoList todos={todos} onCompleted={handleCompleted} />
 
       <pre className="bg-slate-100 p-4 mt-8">{JSON.stringify(todos, null, 2)}</pre>
     </main>

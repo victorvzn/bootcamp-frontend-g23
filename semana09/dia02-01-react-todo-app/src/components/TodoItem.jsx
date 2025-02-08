4ced457571070ea12a526b3d949655aef3cd321d
Victor Villazón <victorzn23@gmail.com>
2025-02-07 19:46:20 -0500

    semana09, dia03, react todo app + tw + continuación

--- semana09/dia02-01-react-todo-app/src/components/TodoItem.jsx
+++ semana09/dia02-01-react-todo-app/src/components/TodoItem.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 const TodoItem = ({ todo, onCompleted, onRemoveTodo }) => {
   // TODO: 02 - Darle funcionalidad al botón remover para quitar una tarea del estado todos.
 
   // TODO: 04 - Editar el titulo del todo haciendo doble click en el titulo, esto hará que aparezca una caja de texto. Y para guardarlo solo debemos presionar enter en la caja de texto.
+
+  const [isEditing, setIsEditing] = useState(false)
+
+
+  const handleCancelOrSave = (event) => {
+    // Averiguar si presionamos la tecla ESC o ENTER u otro más.
+
+    console.log(event.code)
+
+    if (event.code === 'Escape') {
+      console.log('Cancelling...')
+      setIsEditing(false)
+      return
+    }
+
+    if (event.code === 'Enter') {
+      console.log('Saving...')
+      // Hay que guardar el todo editado
+      setIsEditing(false)
+    }
+  }
 
   return (
     <li className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
@@ -11,10 +34,24 @@ const TodoItem = ({ todo, onCompleted, onRemoveTodo }) => {
         checked={todo.completed}
         onChange={onCompleted}
       />
-      <div className="w-full flex justify-between items-center">
-        <span className={`font-medium ${todo.completed ? 'line-through' : ''}`}>
-          {todo.title}
-        </span>
+      <div className="w-full flex justify-between items-center gap-2">
+        {
+          isEditing
+            ? (
+              <input
+                className="w-full border-2 border-yellow-400 p-2 rounded-lg"
+                onKeyDown={handleCancelOrSave}
+              />
+            )
+            : (
+              <span
+                className={`font-medium ${todo.completed ? 'line-through' : ''}`}
+                onDoubleClick={() => setIsEditing(true)}
+              >
+                {todo.title}
+              </span>
+            )
+        }
         <button
           className="bg-red-300 rounded-lg px-1 py-1 cursor-pointer"
           data-id={todo.id}

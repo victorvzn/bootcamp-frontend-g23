aeebff79343e78d2b3ffc9d4974f9c25a265d102
Victor Villazón <victorzn23@gmail.com>
2025-02-12 22:06:12 -0500

    semana10, dia01, react appointments app

--- semana10/dia01-01-react-appointments-app/src/components/appointments/AppointmentList.jsx
+++ semana10/dia01-01-react-appointments-app/src/components/appointments/AppointmentList.jsx
@@ -1,47 +1,76 @@
 const AppointmentList = ({
-  appointments
+  appointments,
+  onConfirm,
+  onEdit,
+  onRemove
 }) => {
-  return (
-    <>
-      <section className="hidden w-full bg-amber-100 rounded-lg">
+
+  if (appointments.length === 0) {
+    return (
+      <section className="w-full bg-amber-100 rounded-lg">
         <h2 className="text-2xl text-center mb-4">Listado de citas médicas</h2>
 
         <div className="flex justify-center items-center h-96 text-3xl">
           No hay citas disponibles.
         </div>
       </section>
+    )
+  }
 
+  return (
+    <>
       <section className=" w-full p-4 bg-amber-100 rounded-lg">
         <h2 className="text-2xl text-center mb-4">Listado de citas médicas</h2>
 
         {/* TODO: 01. Renderizar las citas a partir de la propiedad appointments */}
 
-        <article className="flex flex-col gap-3 mb-4">
-          <div className="border-2 border-sky-500 bg-sky-200 p-3 rounded-lg">
-            <div className="py-2">
-              <h4 className="font-semibold text-xl">Mascota</h4>
-              <div><strong>Nombre:</strong> petName</div>
-              <div><strong>Edad (años):</strong> petAge</div>
-            </div>
+        {appointments.map(appointment => {
+          return (
+            <article className="flex flex-col gap-3 mb-4" key={appointment.id}>
+              <div className="border-2 border-sky-500 bg-sky-200 p-3 rounded-lg">
+                <div className="py-2">
+                  <h4 className="font-semibold text-xl">Mascota</h4>
+                  <div><strong>Nombre:</strong> {appointment.petName}</div>
+                  <div><strong>Edad (años):</strong> {appointment.petAge}</div>
+                </div>
 
-            <div className="py-2">
-              <h4 className="font-semibold text-xl">Dueño</h4>
-              <div><strong>Nombre:</strong> ownerName</div>
-            </div>
+                <div className="py-2">
+                  <h4 className="font-semibold text-xl">Dueño</h4>
+                  <div><strong>Nombre:</strong> {appointment.ownerName}</div>
+                </div>
 
-            <div className="py-2">
-              <h4 className="font-semibold text-xl">Cita</h4>
-              <div><strong>Fecha:</strong> appointmentDate</div>
-              <div><strong>Hora:</strong> appointmentTime</div>
-              <div><strong>Síntomas:</strong> symptoms</div>
-            </div>
-            <div className="flex flex-col gap-3 mt-3">
-              <button className="p-2 bg-green-600 text-white rounded-lg cursor-pointer">Confirmar cita</button>
-              <button className="p-2 bg-sky-600 text-white rounded-lg cursor-pointer">Editar</button>
-              <button className="p-2 bg-red-600 text-white rounded-lg cursor-pointer">Eliminar</button>
-            </div>
-          </div>
-        </article>
+                <div className="py-2">
+                  <h4 className="font-semibold text-xl">Cita</h4>
+                  <div><strong>Fecha:</strong> {appointment.appointmentDate}</div>
+                  <div><strong>Hora:</strong> {appointment.appointmentTime}</div>
+                  <div><strong>Síntomas:</strong> {appointment.symptoms}</div>
+                </div>
+
+                <div className="flex flex-col gap-3 mt-3">
+                  <button
+                    className="p-2 bg-green-600 text-white rounded-lg cursor-pointer"
+                    onClick={() => onConfirm(appointment)}
+                  >
+                    Confirmar cita
+                  </button>
+                  <button
+                    className="p-2 bg-sky-600 text-white rounded-lg cursor-pointer"
+                    onClick={() => onEdit(appointment)}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="p-2 bg-red-600 text-white rounded-lg cursor-pointer"
+                    onClick={() => onRemove(appointment)}
+                  >
+                    Eliminar
+                  </button>
+                </div>
+              </div>
+            </article>
+          )
+        })}
+        
 
         <pre>{JSON.stringify(appointments, null, 2)}</pre>
       </section>
--- semana10/dia01-01-react-appointments-app/src/pages/Appointments.jsx
+++ semana10/dia01-01-react-appointments-app/src/pages/Appointments.jsx
@@ -8,13 +8,38 @@ const Appointments = () => {
   const handleSave = (newAppointment) => {
     console.log('Me estan llamando al guardar el form desde el componente padre', newAppointment)
 
+    // TODO: Manejar el guardado de una cita cuando se está editando
+
     setAppointments([...appointments, newAppointment])
+  }
+
+  const handleRemove = (appointmentToRemove) => {
+    console.log(appointmentToRemove)
+
+    const newAppointments = appointments.filter(
+      appointment => appointment.id !== appointmentToRemove.id
+    )
+
+    setAppointments(newAppointments)
+  }
+
+  // TODO: Añadir funcionalidad al botón editar
+  const handleEdit = (appointmentToEdit) => {
+    console.log(appointmentToEdit)
+  }
+
+  // TODO: Manejar la confirmación y cancelación de una cita (Esta funcionalidad es libre)
+  const handleConfirm = (appointmentToConfirm) => {
+    console.log(appointmentToConfirm)
   }
 
   return (
     <>
       <AppointmentList
         appointments={appointments}
+        onRemove={handleRemove}
+        onEdit={handleEdit}
+        onConfirm={handleConfirm}
       />
 
       <AppointmentForm

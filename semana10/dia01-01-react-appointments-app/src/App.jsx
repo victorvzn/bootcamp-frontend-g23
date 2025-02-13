c8f09ae01504293337dfcf850f242fed2c9a8dd7
Victor Villazón <victorzn23@gmail.com>
2025-02-12 20:04:08 -0500

    semana10, dia01, react appointments app

--- semana10/dia01-01-react-appointments-app/src/App.jsx
+++ semana10/dia01-01-react-appointments-app/src/App.jsx
@@ -48,7 +48,60 @@ const App = () => {
         <section className="w-[400px]">
           <h2 className="text-2xl text-center mb-4">Nuevo paciente</h2>
 
-          
+          <form className="flex flex-col gap-4">
+            <input
+              className="border rounded-lg p-3 shadow-lg"
+              type="text"
+              name="petName"
+              placeholder="Nombre de la mascota. Ej. Firulais"
+              required
+            />
+
+            <input
+              className="border rounded-lg p-3 shadow-lg"
+              type="number"
+              name="petAge"
+              placeholder="Edad de la mascota. Ej. 2"
+              required
+              min="0"
+              max="30"
+            />
+
+            <input
+              className="border rounded-lg p-3 shadow-lg"
+              type="text"
+              name="ownerName"
+              placeholder="Nombre del dueño. Ej. Fabricio"
+              required
+            />
+
+            <input
+              className="border rounded-lg p-3 shadow-lg"
+              type="date"
+              name="appointmentDate"
+              required
+            />
+
+            <input
+              className="border rounded-lg p-3 shadow-lg"
+              type="time"
+              name="appointmentTime"
+              required
+            />
+
+            <textarea
+              className="border rounded-lg p-3 shadow-lg"
+              name="symptoms"
+              placeholder="Síntomas"
+              rows={6}
+            />
+
+            <input
+              className="p-2 bg-green-600 text-white rounded-lg cursor-pointer"
+              type="submit"
+              value="Guardar"
+            />
+          </form>
         </section>
       </main>
     </>

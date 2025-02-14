95f19f1caf42d8d227420a7d0af5cc1143b2530a
Victor Villazón <victorzn23@gmail.com>
2025-02-13 22:20:13 -0500

    semana10, dia02, react pokemon list app, useeffect

--- semana10/dia02-02-react-pokemon-list-pagination-fetch/src/App.jsx
+++ semana10/dia02-02-react-pokemon-list-pagination-fetch/src/App.jsx
@@ -10,12 +10,19 @@ const App = () => {
   }, [])
 
   return (
-    <main>
-      <h1 className="text-3xl text-center">
+    <main className="mt-4">
+      <h1 className="text-3xl text-center mb-4">
         Pokemon List
       </h1>
 
-      {pokemons.map(pokemon => <h3 key={pokemon.url}>{pokemon.name}</h3>)}
+      <section className="pokemons grid grid-cols-3 w-[600px] mx-auto gap-8">
+      {pokemons.map(pokemon => (
+        <article className="pokemon w-[200px] h-[150px] bg-yellow-100 flex flex-col justify-center items-center rounded-lg uppercase shadow" key={pokemon.url}>
+          <img src={pokemon.image} width="80px" style={{ imageRendering: 'pixelated' }} />
+          <h3 className="mt-4 font-medium">{pokemon.name}</h3>
+        </article>
+      ))}
+      </section>
 
       <pre>{JSON.stringify(pokemons, null , 2)}</pre>
     </main>
--- semana10/dia02-02-react-pokemon-list-pagination-fetch/src/services/pokemons.js
+++ semana10/dia02-02-react-pokemon-list-pagination-fetch/src/services/pokemons.js
@@ -1,7 +1,25 @@
-const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=8'
+const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=9'
 
 export const fetchPokemons = async () => {
   const response = await fetch(POKEAPI_URL)
 
-  return await response.json()
+  const data = await response.json()
+
+  const dataResults = data.results.map(pokemon => {
+    const id = pokemon.url.split('/').at(6)
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`
+
+    return {
+      ...pokemon,
+      id,
+      image
+    }
+  })
+
+  // const image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif'
+
+  return {
+    ...data,
+    results: dataResults
+  }
 }

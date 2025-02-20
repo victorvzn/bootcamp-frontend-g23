0c1367337beeea1cd159405b96a6fabcfb832f49
Victor Villazón <victorzn23@gmail.com>
2025-02-19 22:30:47 -0500

    dia01, react router componentes

--- semana11/dia01-02-react-router-components/src/components/home-page/CharacterList.jsx
+++ semana11/dia01-02-react-router-components/src/components/home-page/CharacterList.jsx
@@ -1,8 +1,34 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { fetchCharacters } from "../../services/characters"
+
 const CharacterList = () => {
   // TODO: Renderizar los items del resultado de personajes en un grilla de 3x3
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetchCharacters()
+      .then(results => setCharacters(results.items))
+  }, [])
   
   return (
-    <div>CharacterList</div>
+    <>
+      <section className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-6">
+        {characters.map(character => {
+          return (
+            <article
+              key={character.id}
+              className="bg-yellow-300 rounded-lg p-3 flex flex-col justify-evenly items-center"
+            >
+              <img src={character.image} width={50} />
+              <div className="font-bold text-3xl text-center mt-2">#{character.id} - {character.name}</div>
+              <div className="font-bold text-center mt-2 bg-orange-300 text-slate-700 p-2 uppercase rounded-lg">{character.race}</div>
+            </article>
+          )
+        })}
+      </section>
+    </>
+  // <pre>{JSON.stringify(characters, null, 2)}</pre>
   )
 }
 

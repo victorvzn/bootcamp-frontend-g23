758df608f17a3486524579dc0e90f5afad10e05b
Victor Villazón <victorzn23@gmail.com>
2025-02-19 23:07:09 -0500

    dia01, react router componentes

--- semana11/dia01-02-react-router-components/src/pages/CharacterPage.jsx
+++ semana11/dia01-02-react-router-components/src/pages/CharacterPage.jsx
@@ -1,6 +1,41 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { Link, useNavigate, useParams } from "react-router"
+
+import { getCharacter } from "../services/characters"
+
 const CharacterPage = () => {
+  const { id } =  useParams() // { id: 123 }
+
+  const navigate = useNavigate()
+
+  const [character, setCharacter] = useState()
+
+  useEffect(() => {
+    getCharacter(id)
+      .then(result => setCharacter(result))
+  }, [])
+
+  const handleBackHome = () => {
+    navigate('/')
+  }
+
   return (
-    <div>CharacterPage</div>
+    <>
+      <h1>Character detail #{id}</h1>
+
+      <section>
+        <button onClick={handleBackHome}>Back home</button>
+
+        <Link to='/'>Back Home</Link>
+
+        <img src={character?.image} width={200} />
+        <h2>{character?.race}</h2>
+        <strong>{character?.originPlanet?.name}</strong>
+        <p>{character?.description}</p>
+
+      </section>
+    </>
   )
 }
 

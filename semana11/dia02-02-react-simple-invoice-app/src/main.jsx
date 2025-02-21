3a5ad4cc9be8541af019f95268322bd7a5c9cce1
Victor Villazón <victorzn23@gmail.com>
2025-02-20 21:41:06 -0500

    semana11, dia02, react simple invoice app

--- semana11/dia02-02-react-simple-invoice-app/src/main.jsx
+++ semana11/dia02-02-react-simple-invoice-app/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import { Login } from './pages/Login'
+import { Home } from './pages/Home'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+
+      <Route path='/home' element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
 )

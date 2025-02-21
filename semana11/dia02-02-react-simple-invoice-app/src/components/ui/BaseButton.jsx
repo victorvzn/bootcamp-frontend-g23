665ae37d27872b2ab1fc11b2e773f3795bf39701
Victor Villazón <victorzn23@gmail.com>
2025-02-20 21:56:16 -0500

    semana11, dia02, react simple invoice app

---
+++ semana11/dia02-02-react-simple-invoice-app/src/components/ui/BaseButton.jsx
@@ -0,0 +1,17 @@
+export const BaseButton = ({
+  type='button',
+  label,
+  bgColor,
+  textColor = 'text-white',
+  classx,
+  onClick
+}) => {
+  return (
+    <button
+      type={type}
+      className={`rounded-full px-5 py-4 font-semibold min-w-[100px] ${bgColor} ${textColor} ${classx}`}
+    >
+      Save
+    </button>
+  )
+}
--- semana11/dia02-02-react-simple-invoice-app/src/pages/Login.jsx
+++ semana11/dia02-02-react-simple-invoice-app/src/pages/Login.jsx
@@ -1,3 +1,35 @@
+import { BaseButton } from "../components/ui/BaseButton"
+import { BaseInput } from "../components/ui/BaseInput"
+
 export const Login = () => {
-  return <h1>Login page 2</h1>
+  return (
+    <main className="w-[400px] m-auto flex flex-col gap-5">
+      <div className="bg-slate-600 p-8 rounded-lg flex flex-col gap-6">
+        <h2 className="text-white text-center text-4xl font-bold">Invoice app</h2>
+
+        <p className="text-white font-light text-center">Ingresa un nombre de usuario y password</p>
+
+        <form>
+          <BaseInput
+            label="Username"
+            placeholder="jhondoe"
+            name="username"
+          />
+          <BaseInput
+            type="password"
+            label="Password"
+            placeholder="*************"
+            name="password"
+          />
+
+          <BaseButton
+            type="submit"
+            label='Login'
+            bgColor='bg-violet-500'
+            classx='w-full hover:bg-violet-600 duration-300 cursor-pointer'
+          />
+        </form>
+      </div>
+    </main>
+  )
 }

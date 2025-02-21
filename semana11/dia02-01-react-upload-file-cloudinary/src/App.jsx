5e83b7ff1bede79cbe1ce46414106c405044ba19
Victor Villazón <victorzn23@gmail.com>
2025-02-20 19:43:31 -0500

    semana11, dia02, react upload file en cloudinary

--- semana11/dia02-01-react-upload-file-cloudinary/src/App.jsx
+++ semana11/dia02-01-react-upload-file-cloudinary/src/App.jsx
@@ -1,8 +1,27 @@
+const CLOUD_NAME = "dtmarfdry"
+const UPLOAD_PRESET = "g23-semana11"
+const FOLDER_NAME = "g23-semana11"
+
+const CloudinaryUploadImage = () => {
+  return (
+    <label>
+      Upload an image
+      <input
+        type="file"
+      />
+    </label>
+  )
+}
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold">
-      Upload file + Cloudinary
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold">
+        Upload file + Cloudinary
+      </h1>
+
+      <CloudinaryUploadImage />
+    </>
   )
 }
 

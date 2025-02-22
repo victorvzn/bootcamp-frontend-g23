ed52297f74a5235e2e69ce6b49a36aa30eb37196
Victor Villazón <victorzn23@gmail.com>
2025-02-21 20:33:37 -0500

    semana11, dia02, react simple invoice app

--- semana11/dia02-02-react-simple-invoice-app/src/pages/ProductsList.jsx
+++ semana11/dia02-02-react-simple-invoice-app/src/pages/ProductsList.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react"
+import { useAuth } from "../hooks/useAuth"
 
-const ENDPOINT_URL = 'https://dummyjson.com/products'
+const ENDPOINT_URL = 'https://dummyjson.com/auth/products'
 
-const fetchProducts = async () => {
-  const response = await fetch(ENDPOINT_URL)
-
+const fetchProducts = async (accessToken) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  }
+  
+  const response = await fetch(ENDPOINT_URL, options)
+  
   return await response.json()
 }
 
 const ProductsList = () => {
+  const { user } = useAuth()
+
+  console.log(user)
+
   // TODO: Renderizar los productos del endpoint https://dummyjson.com/products
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    fetchProducts()
+    fetchProducts(user.accessToken)
       .then((data) => {
         console.log(data.products)
         setProducts(data.products)
@@ -25,7 +37,7 @@ const ProductsList = () => {
       <h2 className="text-xl font-bold">ProductsList</h2>
 
       <ul>
-        {products.map(product => {
+        {products && products.map(product => {
           return (
             <li key={product.id}>* {product.title}</li>
           )

0122ab9cb5f4beddedeef2e9a85b66c1f191151a
Victor Villazón <victorzn23@gmail.com>
2025-02-21 22:46:21 -0500

    semana11, dia02, react simple invoice app

--- semana11/dia02-02-react-simple-invoice-app/src/components/invoices/List.jsx
+++ semana11/dia02-02-react-simple-invoice-app/src/components/invoices/List.jsx
@@ -7,29 +7,40 @@ const List = ({ invoices }) => {
     <main
       className="md:w-[740px] px-2 mx-auto flex flex-col gap-5"
     >
-      <article
-        className="bg-white px-4 py-4 rounded-lg text-base shadow md:flex grid grid-cols-2 justify-between items-center gap-3"
-      >
-        <div className="md:w-20 text-zinc-800 text-center">
-          <span className="text-slate-400">#</span>
-          <span className="font-extrabold uppercase">XYZ123</span>
-        </div>
-        <div className="md:w-44 font-semibold text-zinc-500 text-center">date</div>
-        <div className="md:w-44 font-semibold text-zinc-500 text-center">fullName</div>
-        <div className="md:w-44 font-extrabold text-3xl text-zinc-800 text-center">$1500.00</div>
-        <div>
-          <BaseTag label="paid" bgColorClass='bg-emerald-500/20' textColorClass='text-emerald-500' />
-          {/* <BaseTag label="pending" bgColorClass='bg-orange-500/20' textColorClass='text-orange-500' /> */}
-          {/* <BaseTag label="draft" bgColorClass='bg-slate-500/20' textColorClass='text-slate-500' /> */}
-        </div>
-        <div
-          className="md:w-10 flex justify-center"
+      {invoices && invoices.map(invoice => (
+
+        <article
+          className="bg-white px-4 py-4 rounded-lg text-base shadow md:flex grid grid-cols-2 justify-between items-center gap-3"
+          key={invoice.id}
         >
-          <Link to={`/invoices/1`} className="p-3 rounded-full text-zinc-500 hover:bg-zinc-100 duration-300">
-            <TbChevronRight />
-          </Link>
-        </div>
-      </article>
+          <div className="md:w-20 text-zinc-800 text-center">
+            <span className="text-slate-400">#</span>
+            <span className="font-extrabold uppercase">{invoice.code}</span>
+          </div>
+          <div className="md:w-44 font-semibold text-zinc-500 text-center">{invoice.date}</div>
+          <div className="md:w-44 font-semibold text-zinc-500 text-center">{invoice.fullName}</div>
+          <div className="md:w-44 font-extrabold text-3xl text-zinc-800 text-center">
+            {invoice.currencySymbol}
+            {invoice.total}
+          </div>
+          <div>
+            {invoice.status === 'paid'
+              && <BaseTag label={invoice.status} bgColorClass='bg-emerald-500/20' textColorClass='text-emerald-500' /> }
+            {invoice.status === 'pending'
+              && <BaseTag label={invoice.status} bgColorClass='bg-orange-500/20' textColorClass='text-orange-500' /> }
+            {invoice.status === 'draft'
+              && <BaseTag label={invoice.status} bgColorClass='bg-slate-500/20' textColorClass='text-slate-500' /> }
+          </div>
+          <div
+            className="md:w-10 flex justify-center"
+          >
+            <Link to={`/invoices/1`} className="p-3 rounded-full text-zinc-500 hover:bg-zinc-100 duration-300">
+              <TbChevronRight />
+            </Link>
+          </div>
+        </article>
+      ))}
+      
     </main>
   )
 }

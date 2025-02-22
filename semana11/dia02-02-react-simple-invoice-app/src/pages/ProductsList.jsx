d182a31724bdcbd8188fa7cea6caf11c6cdf074b
Victor Villazón <victorzn23@gmail.com>
2025-02-21 20:22:25 -0500

    semana11, dia02, react simple invoice app

--- semana11/dia02-02-react-simple-invoice-app/src/pages/ProductsList.jsx
+++ semana11/dia02-02-react-simple-invoice-app/src/pages/ProductsList.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react"
+
+const ENDPOINT_URL = 'https://dummyjson.com/products'
+
+const fetchProducts = async () => {
+  const response = await fetch(ENDPOINT_URL)
+
+  return await response.json()
+}
+
 const ProductsList = () => {
   // TODO: Renderizar los productos del endpoint https://dummyjson.com/products
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then((data) => {
+        console.log(data.products)
+        setProducts(data.products)
+      })
+  }, [])
 
   return (
-    <div>ProductsList</div>
+    <main>
+      <h2 className="text-xl font-bold">ProductsList</h2>
+
+      <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>* {product.title}</li>
+          )
+        })}
+      </ul>
+    </main>
   )
 }
 

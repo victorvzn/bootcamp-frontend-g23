21137e0410dabb94a7ac4d4d11bac89e34669cb0
Victor Villazón <victorzn23@gmail.com>
2025-02-26 21:01:53 -0500

    semana12, dia01, react redux counter and shopping cart app

--- semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ProductList.jsx
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ProductList.jsx
@@ -1,6 +1,5 @@
 // TODO: Listar los productos en lista como una grilla de 1 columna con botones que contengan el title y el thumbnail
 
-
 export default function ProductList({ products }) {
   return (
     <section className="flex flex-col p-4">

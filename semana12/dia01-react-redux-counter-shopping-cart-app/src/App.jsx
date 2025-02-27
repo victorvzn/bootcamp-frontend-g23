0eaa067b90351667bb4a5784cc9584bb0f80f643
Victor Villazón <victorzn23@gmail.com>
2025-02-26 20:36:08 -0500

    semana12, dia01, react redux counter and shopping cart app

--- semana12/dia01-react-redux-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react"
 import { Counter } from "./components/Counter"
+import ProductList from "./components/ProductList"
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then((data) => setProducts(data.products))
+  }, [])
+
   return (
     <>
+      <ProductList products={products} />
+      {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
       <Counter />
     </>
   )
---
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ProductList.jsx
@@ -0,0 +1,8 @@
+// TODO: Listar los productos en lista como una grilla de 1 columna con botones que contengan el title y el thumbnail
+
+
+export default function ProductList({ products }) {
+  return (
+    <div>ProductList</div>
+  )
+}

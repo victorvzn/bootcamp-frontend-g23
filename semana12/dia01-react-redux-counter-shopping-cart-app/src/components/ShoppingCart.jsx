9be3021874d8863c75a1453064fa7de64725c71f
Victor Villazón <victorzn23@gmail.com>
2025-02-26 21:43:00 -0500

    semana12, dia01, react redux counter and shopping cart app

--- semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,5 +1,23 @@
 export const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
+    <section className="w-52">
+      <h3 className="text-2xl mb-2">Shopping Cart</h3>
+
+      <div className="mb-2">
+        <button className="bg-violet-400 p-2 min-w-14 rounded-lg cursor-pointer text-white w-full">Clean cart</button>
+      </div>
+
+      <ul>
+        <li className="flex flex-col gap-2 font-bold bg-slate-100  p-2 rounded-lg">
+          <span>title</span>
+          <span>S/ 0.00 (qty: 0)</span>
+          <button
+            className="bg-red-400 p-1 rounded-lg cursor-pointer text-white"
+          >
+            ❌
+          </button>
+        </li>
+      </ul>
+    </section>
   )
 }

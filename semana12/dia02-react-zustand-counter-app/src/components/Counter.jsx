cb8d4a5ddce901e6c3147bcbfd906acd1dc3e225
Victor Villazón <victorzn23@gmail.com>
2025-02-27 20:53:56 -0500

    semana12, dia01, react redux counter and shopping cart app

--- semana12/dia02-react-zustand-counter-app/src/components/Counter.jsx
+++ semana12/dia02-react-zustand-counter-app/src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import { useCounterStore } from '../store/counter'
 
 export const Counter = () => {
-  const { count, increment, decrement } = useCounterStore()
+  const { count, increment, decrement, incrementByValue } = useCounterStore()
 
   return (
     <div className="border rounded-lg p-2 flex flex-col items-center gap-2">
@@ -10,6 +10,7 @@ export const Counter = () => {
       <div className="flex items-center gap-2">
         <button
           className="bg-amber-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+          onClick={() => incrementByValue(5)}
         >+5</button>
         <button
           className="bg-blue-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
@@ -24,6 +25,7 @@ export const Counter = () => {
         >-1</button>
         <button
           className="bg-amber-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+          onClick={() => incrementByValue(-5)}
         >-5</button>
       </div>
     </div>
